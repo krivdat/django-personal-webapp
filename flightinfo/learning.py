439a813f1e24c6e28bf0dee20b0bb86abf27a8e1
krivdat <krivdat@gmail.com>
2018-07-23 18:03:59 +0200

    todays work on flightinfo app

--- flightinfo/learning.py
+++ flightinfo/learning.py
@@ -6,25 +6,26 @@ locations = { # lamin,lomin,lamax,lomax
     "Prague": [49.8604,14.0617,50.2457,14.9653],
 }
 
-# all records for Switzerland
 loc = "Prague"
 query = "https://opensky-network.org/api/states/all?" \
     + "lamin=" + str(locations[loc][0]) \
     + "&lomin=" + str(locations[loc][1]) \
     + "&lamax=" + str(locations[loc][2]) \
     + "&lomax=" + str(locations[loc][3])
-print(query)
+# print(query)
 
 response = requests.get(query)
 respjson = json.loads(response.text)
 
-print("Found {} results for location {}.\n".format(len(respjson['states']),loc))
+print("Found {} results for location {}.".format(len(respjson['states']),loc))
+print("List of aircrafts not on ground:\n")
 
 for record in respjson['states']:
-    print("Callsign: {}".format(record[1]))
-    print("Country: {}".format(record[2]))
-    print("Speed (km/h): {}".format(record[9]*3.6))
-    print("Altitude (m): {}".format(record[7]))
-    print("On ground: {}\n".format(record[8]))
+    if record[8] is not True:
+        print("Callsign: {}".format(record[1]))
+        print("Country: {}".format(record[2]))
+        print("Speed (km/h): {:.0f}".format(record[9]*3.6))
+        print("Altitude (m): {:.0f}\n".format(record[7]))
+        # print("On ground: {}\n".format(record[8]))
 
 # print(respjson['states'][0])
--- flightinfo/views.py
+++ flightinfo/views.py
@@ -1,3 +1,26 @@
+import json
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    locations = { # lamin,lomin,lamax,lomax
+        "Switzerland": [45.8389,5.9962,47.8229,10.5226],
+        "Prague": [49.8604,14.0617,50.2457,14.9653],
+    }
+
+    loc = "Prague"
+    query = "https://opensky-network.org/api/states/all?" \
+        + "lamin=" + str(locations[loc][0]) \
+        + "&lomin=" + str(locations[loc][1]) \
+        + "&lamax=" + str(locations[loc][2]) \
+        + "&lomax=" + str(locations[loc][3])
+    # print(query)
+
+    response = requests.get(query)
+    respjson = json.loads(response.text)
+    flightinfo = []
+    for record in respjson['states']:
+        if record[8] is not True: # get only planes not on the ground
+            flightinfo.append(record)
+            
+    return render(request, 'flightinfo/base.html', {'flightinfo': flightinfo, 'loc': loc})
