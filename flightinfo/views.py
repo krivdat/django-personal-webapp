ddbd64561ee9aa74bd9a2f3f23ef214976e931a0
krivdat <krivdat@gmail.com>
2018-07-24 17:44:09 +0200

    Added location selection by URL plus some cosmetic changes.

--- flightinfo/views.py
+++ flightinfo/views.py
@@ -2,13 +2,12 @@ import json
 import requests
 from django.shortcuts import render
 
-def index(request):
+def index(request, loc='Prague'):
     locations = { # lamin,lomin,lamax,lomax
         "Switzerland": [45.8389,5.9962,47.8229,10.5226],
         "Prague": [49.8604,14.0617,50.2457,14.9653],
     }
 
-    loc = "Prague"
     query = "https://opensky-network.org/api/states/all?" \
         + "lamin=" + str(locations[loc][0]) \
         + "&lomin=" + str(locations[loc][1]) \
@@ -20,11 +19,31 @@ def index(request):
     flights = []
     for record in respjson['states']:
         if record[8] is not True: # get only planes not on the ground
+            if record[13] is not None:
+                altitude = int(record[13])
+            else:
+                altitude = "---"
+            if record[9] is not None:
+                speed = int(record[9]*3.6)
+            else:
+                speed = "---"
+            if record[11] is not None:
+                verticalrate = record[11]
+                if verticalrate > 0:
+                    status = "rise"
+                elif verticalrate < 0:
+                    status = "descent"
+                else:
+                    status = ""
+            else:
+                verticalrate = "---"
+                status = ""
             flights.append({
-                'Callsign':record[1],
-                'Country':record[2],
-                'Speed':int(record[9]*3.6),
-                'Altitude':int(record[7]),
-                'VerticalRate':record[11] })
+                'Callsign': record[1],
+                'Country': record[2],
+                'Speed': speed,
+                'Altitude': altitude,
+                'VerticalRate': verticalrate,
+                'Status': status })
 
     return render(request, 'flightinfo/base.html', {'flights': flights, 'loc': loc})
